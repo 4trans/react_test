bc42f14d3665c94f270a354f05a9138984281c8d
Ilja Kartašov <ik@lowenware.com>
2023-01-11 15:36:24 +0100

    Add router and 2 lazy loaded modules

--- packages/info-system/src/App.tsx
+++ packages/info-system/src/App.tsx
@@ -1,10 +1,40 @@
 import './App.css';
 import Header from '@4trans/shared/header/Header'
+import { Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+
+const Home = React.lazy(() => import('./modules/home'));
+const About = React.lazy(() => import('./modules/about'));
 
 function App() {
   return (
     <div className="App">
       <Header title="Info-System"></Header>
+      <nav>
+        <Link to="/">Home</Link>
+        {` | `}
+        <Link to="/about">About</Link>
+      </nav>
+
+
+        <Routes>
+          <Route
+            index
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Home />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="about"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <About />
+              </React.Suspense>
+            }
+          />
+        </Routes>
     </div>
   );
 }
